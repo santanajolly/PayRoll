cd7e44982446492fd8b6fb2e745c38ed556b4264
Santana Jolly <santanaharris@gmail.com>
2019-10-01 09:14:08 -0400

    Add project files.

---
+++ Data/PayrollContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PayRoll.Models;
+
+
+namespace PayRoll.Data
+{
+    public class PayrollContext : DbContext
+    {
+        public PayrollContext(DbContextOptions<PayrollContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PayRoll.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public float RateOfPay { get; set; }
+
+        public float HoursWorked { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime PayWeek { get; set; }
+    }
+}
